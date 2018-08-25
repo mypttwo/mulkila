05d9cf6733c3d53cab75a4abb75f7c487c0c0db1
Random Guy <mypt.two@gmail.com>
2018-08-25 08:52:08 +0530

    Simplify rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -5,9 +5,9 @@ let rules = () => {
         <div>
         <p className="card-text">Every game starts with a Jackpot. </p>
         <p className="card-text">You can only bet a fixed value to win the Jackpot.</p>
-        <p className="card-text">If another player after you, your bet is added to the Jackpot. Your bet will not be refunded.</p>
+        <p className="card-text">If another player places a bet after you, your bet is added to the Jackpot. Your bet will not be refunded.</p>
         <p className="card-text">You can bet as many times as you wish!</p>
-        <p className="card-text">At some unknown point of time the betting will cease. The player with the latest bet will win the Jackpot.</p>
+        <p className="card-text">At some unknown point of time the betting will cease. The player with the last bet will win the Jackpot.</p>
         <p className="card-text">Thats it.</p>
         </div>
     );
